629772ac8ef835528086dd0020e716752b32a5f7
AritificialPhysics <sanskar10100@gmail.com>
2021-02-02 21:34:38 +0530

    User Auth with Firebase UI

--- app/src/main/java/com/example/android/asklepius/MainActivity.java
+++ app/src/main/java/com/example/android/asklepius/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.android.asklepius;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+	// Choose authentication providers
+	List<AuthUI.IdpConfig> providers = Arrays.asList(
+			new AuthUI.IdpConfig.GoogleBuilder().build());
+
+	private int RC_SIGN_IN = 1;
+	private static final String TAG = "MainActivity";
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		startActivityForResult(
+				AuthUI.getInstance()
+						.createSignInIntentBuilder()
+						.setAvailableProviders(providers)
+						.build(),
+						RC_SIGN_IN);
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == RC_SIGN_IN) {
+			IdpResponse response = IdpResponse.fromResultIntent(data);
+
+			if (resultCode == RESULT_OK) {
+				// Successfully signed in
+				FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+				Log.d(TAG, "onActivityResult: Sign In Successful!");
+			} else {
+				Log.d(TAG, "onActivityResult: Sign In Failed");
+			}
+		}
 	}
 }
