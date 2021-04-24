c84dca4d2f425380ca5adb45dd1099eb1608ca82
Sanskar Agrawal <sanskar10100@gmail.com>
2021-04-24 12:39:42 +0530

    User Validation UI Improved and backend documented.

--- app/src/main/java/com/example/android/asklepius/UserValidationUpload.java
+++ app/src/main/java/com/example/android/asklepius/UserValidationUpload.java
@@ -1,5 +1,6 @@
 package com.example.android.asklepius;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -36,7 +37,6 @@ import java.io.IOException;
 
 public class UserValidationUpload extends AppCompatActivity {
 
-    private boolean unregistered = true;
     private static final String TAG = "UserValidationUpload";
     private File imageFile;
     private String imageURL;
@@ -48,6 +48,7 @@ public class UserValidationUpload extends AppCompatActivity {
     private static final int PICK_IMAGE = 313;
     private Snackbar uploadingSnackBar;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,6 +68,11 @@ public class UserValidationUpload extends AppCompatActivity {
         storage = FirebaseStorage.getInstance().getReference();
     }
 
+    /**
+     * If the user chooses to take the image through the camera, this method generates an
+     * empty file in the external app directory, and loads the resultant image in that file
+     * which gives us a URI which can be used to upload that image to Firebase
+     */
     public void onTakeIDImageButtonClicked(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -81,6 +87,10 @@ public class UserValidationUpload extends AppCompatActivity {
         startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
     }
 
+    /**
+     * If the user chooses to select an image from the gallery, instantiate and fire an intent
+     * to do so.
+     */
     public void onSelectIDImageButtonClicked(View view) {
         Intent intent = new Intent();
         intent.setType("image/*");
@@ -88,6 +98,10 @@ public class UserValidationUpload extends AppCompatActivity {
         startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE);
     }
 
+    /**
+     * @return a new File object created for storing the returned data from the Camera API
+     *         after an image capture.
+     */
     private File createImageFile() {
         String imageFileName = "JPEG_" + FirebaseAuth.getInstance().getCurrentUser().getUid();
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
@@ -106,6 +120,17 @@ public class UserValidationUpload extends AppCompatActivity {
         return image;
     }
 
+    /**
+     * Creates a reference to a Firebase Storage location based on the current user ID to prevent
+     * duplicate uploads of the same ID in the future thus saving space. After a reference is created,
+     * the imageURI (obtained either from the generated file after image capture from Camera) or a file
+     * selected from the gallery is used to upload the file to the Firebase Storage. Until the file is
+     * successfully uploaded, a SnackBar and ProgressBar help the user ascertain the upload progress.
+     * When the file is uploaded, a download URL is obtained and the submit button is enabled.
+     * A error is displayed if the upload fails for some reason
+     *
+     * Note: The uploaded file is completely uncompressed to account for improper angles.
+     */
     private void uploadImage() {
         String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
         StorageReference imageReference = storage.child("images/" + userID);
@@ -142,6 +167,7 @@ public class UserValidationUpload extends AppCompatActivity {
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
+                Toast.makeText(UserValidationUpload.this, "The image failed to upload!", Toast.LENGTH_SHORT).show();
                 Log.d(TAG, "onFailure: Image failed to upload. The following exception occurred: " );
                 e.printStackTrace();
             }
@@ -149,6 +175,12 @@ public class UserValidationUpload extends AppCompatActivity {
 
     }
 
+    /**
+     * The first condition handles an image capture by the camera, in which case imageURI is already
+     * set. It's simply a matter of displaying an informative SnackBar and triggering the upload.
+     * The second condition handles the case in which the image is selected from the gallery,
+     * in which case we just obtain the URI from the returned intent and trigger upload.
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -166,6 +198,9 @@ public class UserValidationUpload extends AppCompatActivity {
         }
     }
 
+    /**
+     * Back button is disabled until the details are submitted
+     */
     @Override
     public void onBackPressed() {
         if (uploaded) {
@@ -175,6 +210,14 @@ public class UserValidationUpload extends AppCompatActivity {
         }
     }
 
+    /**
+     * This button is enabled when the image upload is successfully completed. If the user is new,
+     * that is there is no presence in the database, and a new child is created and the user object is pushed.
+     *
+     * If the user already exists in the database, the complete list of users is fetched from the database once,
+     * and the existing used object at the matching key is replace by a new one. The image is simply replaced at
+     * the same ID, so there is no duplication.
+     */
     public void onSubmitValidationInfoButtonClicked(View view) {
         TextInputLayout icmrIDTextField = findViewById(R.id.textField_ICMR_ID);
         String icmrId = icmrIDTextField.getEditText().getText().toString().trim();
@@ -183,11 +226,7 @@ public class UserValidationUpload extends AppCompatActivity {
             return;
         }
 
-        if (user != null) {
-            unregistered = false;
-        }
-
-        if (unregistered) {
+        if (user == null) {
             User user = new User(imageURL, FirebaseAuth.getInstance().getUid(), icmrId, Values.userVerificationState.Pending.toString(), "",
                     FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
             DatabaseReference userDB = Values.userDB;
@@ -230,7 +269,7 @@ public class UserValidationUpload extends AppCompatActivity {
             };
             userDB.addListenerForSingleValueEvent(listener);
 
-            if (uploaded == true) {
+            if (uploaded) {
                 userDB.removeEventListener(listener);
             }
         }
