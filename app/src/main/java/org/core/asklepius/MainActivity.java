a34b624c61d89cc86046ba347cb36dda82cfb02b
Sanskar Agrawal <sanskar10100@gmail.com>
2021-05-06 12:49:53 +0530

    Fixed an import that was crashing the app

--- app/src/main/java/org/core/asklepius/MainActivity.java
+++ app/src/main/java/org/core/asklepius/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
